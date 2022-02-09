6a00dcd3fff893be0338a143b326279777a85896
Ivan <chakin.2001@mail.ru>
2022-02-09 11:58:59 +0300

    add Card

---
+++ src/components/UI/Card.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="card">
+    <span class="card-name">{{ name }}</span>
+    <span class="card-title">{{ title }}</span>
+    <div class="card-body">
+      card-body
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Card",
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    title: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
