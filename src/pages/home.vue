e6358b0954c039959edc7bc541c9593312b72d90
Ivan <chakin.2001@mail.ru>
2022-02-09 11:59:59 +0300

    change home

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,15 +1,38 @@
 <template>
   <p>Hello home!</p>
   <p>Hello home2</p>
+  <div class="card__wrapper" v-for="item in items" :key="item.id">
+    <Card :name="`${item.lvl} lvl`" :title="item.title" />
+  </div>
 </template>
 
 <script>
+import Card from '@/components/UI/Card'
+
 export default {
-  name: "home"
+  name: "home",
+  components: {Card},
+  data() {
+    return {
+      items: [
+        {
+          id: 1,
+          title: 'archer',
+          descr: 'archer archer archer archer',
+          lvl: 4
+        },
+        {
+          id: 2,
+          title: 'Wizard',
+          descr: 'Wizard Wizard Wizard Wizard',
+          lvl: 5
+        }
+      ]
+    }
+  }
 }
 </script>
 
 <style scoped>
 
 </style>
-\
